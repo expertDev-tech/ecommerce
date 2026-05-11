bafdb104a7698d13b6f950b96b7e1b40d595102c
Ankit Parmar <parmarankit477@gmail.com>
2026-05-11 15:53:39 +0530

    SMTP

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::middleware('auth')->get('/dashboard-test', function () {
+    return 'Protected Page';
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
